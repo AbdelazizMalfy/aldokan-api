960950b10e4c71cdde0c946d3646a650b1d86315
abdelazizmalfy <abdelazizmalfy@gmail.com>
2024-01-01 22:18:10 +0100

    allow origin all

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,7 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: 'https://yourfrontenddomain.com',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      origin: '*',
     },
   });
   const config = app.get(ConfigService);
